024f1beff8687307d5247b73e5081bf3e4fe6bc5
hughes <hughes036@gmail.com>
2023-12-22 23:49:54 -0500

    first test for model, formatting

--- src/complexity/bifurcation/model.py
+++ src/complexity/bifurcation/model.py
@@ -6,11 +6,16 @@ from complexity.bifurcation.i_map_func_factory import IMapFuncFactory
 
 class Model:
     def __init__(
-        self, map_func_factory: IMapFuncFactory, r_increment=0.01, init_x_val=0.5, iterations=1000, orbits_to_skip=500
+        self,
+        map_func_factory: IMapFuncFactory,
+        r_increment=0.01,
+        init_x_val=0.5,
+        iterations=1000,
+        orbits_to_skip=500,
     ):
         self._r_orbits = {}
         for r in np.arange(0.1, 4.1, r_increment):
-            x_vals = self.iterate(
+            x_vals = self._iterate(
                 map_func=map_func_factory.create(
                     r
                 ),  #  map type needs to be passed in constructor,
@@ -20,7 +25,7 @@ class Model:
             )
             self._r_orbits[r] = x_vals
 
-    def iterate(self, map_func: IMapFunc, orbits_to_skip, x_init, iterations):
+    def _iterate(self, map_func: IMapFunc, orbits_to_skip, x_init, iterations):
         x_vals = []
         x = x_init
         for i in range(iterations):
---
+++ tests/complexity/bifurcation/test_model.py
@@ -0,0 +1,22 @@
+from complexity.bifurcation.logistic_map_func_factory import LogisticMapFuncFactory
+from complexity.bifurcation.model import Model
+
+
+def test_logistic_map_model():
+    model = Model(
+        LogisticMapFuncFactory(), r_increment=2, iterations=10, orbits_to_skip=5
+    )
+    assert model.get_r_orbits() == {
+        0.1: [
+            2.4309017497646033e-08,
+            2.4309016906717706e-09,
+            2.4309016847624875e-10,
+            2.4309016841715593e-11,
+        ],
+        2.1: [
+            0.5238095250269179,
+            0.5238095236877844,
+            0.5238095238216979,
+            0.5238095238083065,
+        ],
+    }
