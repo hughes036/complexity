2eccc743f022110f8baa5d2906138ea2e1cdf798
hughes <hughes036@gmail.com>
2023-12-26 18:00:38 -0500

    OO bifurcation diagram

--- src/complexity/bifurcation/bifurcation.py
+++ src/complexity/bifurcation/bifurcation.py
@@ -8,45 +8,48 @@ from complexity.bifurcation.logistic.logistic_map_func_factory import (
 
 from complexity.bifurcation.model import Model
 
+class BifurcationDiagram():
 
-def plot():
-    # model: Model = Model(LogisticMapFuncFactory())
-    model: Model = Model(CubicMapFuncFactory(), init_y_val=.9, iterations=1000, orbits_to_skip=800)
-    fig, (ax1, ax2) = plt.subplots(2)
-    fig.suptitle('Bifurcation Diagram')
-    ax1.set_title('R Plot')
-    ax2.set_title('Time Plot')
+    def init(self):
+        pass
 
-    for r, x_vals in model.get_r_orbits().items():
-        ax1.scatter(
-            x=list(
-                map(lambda x: r, x_vals)
-            ),  # A list of the r value (repeated) the same length as x_vals
-            y=x_vals,
-            s=list(
-                map(lambda s: 0.05, x_vals)
-            ),  # A list of the size of the dots (repeated) the same length as x_vals
-            c = '#347deb',
-        )
+    def plot(self):
+        # model: Model = Model(LogisticMapFuncFactory())
+        model: Model = Model(CubicMapFuncFactory(), init_y_val=.9, iterations=1000, orbits_to_skip=800)
+        fig, (ax1, ax2) = plt.subplots(2)
+        fig.suptitle('Bifurcation Diagram')
+        ax1.set_title('R Plot')
+        ax2.set_title('Time Plot')
 
-
-    def on_click(event):
-        if event.inaxes == ax1:
-            ax2.clear()
-            ax2.set_title('Time Plot')
-            closest_r = model.closest_r_value(event.xdata) 
-            y_vals = model.get_r_orbits()[closest_r][-50:]
-            x_vals = list(range(0, len(y_vals)))
-            ax2.plot(x_vals, y_vals, label=f'r={event.xdata}')
-            plt.draw()
-
-    fig.canvas.mpl_connect('button_press_event', on_click)
-    plt.show()
+        for r, x_vals in model.get_r_orbits().items():
+            ax1.scatter(
+                x=list(
+                    map(lambda x: r, x_vals)
+                ),  # A list of the r value (repeated) the same length as x_vals
+                y=x_vals,
+                s=list(
+                    map(lambda s: 0.05, x_vals)
+                ),  # A list of the size of the dots (repeated) the same length as x_vals
+                c = '#347deb',
+            )
 
 
-def main():
-    plot()
+        def on_click(event):
+            if event.inaxes == ax1:
+                ax2.clear()
+                closest_r = model.closest_r_value(event.xdata)
+                ax2.set_title(f'Time Plot: {closest_r}')
+                y_vals = model.get_r_orbits()[closest_r][-50:]
+                x_vals = list(range(0, len(y_vals)))
+                print(y_vals)
+                ax2.plot(x_vals, y_vals, label=f'r={event.xdata}')
+                plt.draw()
+
+        fig.canvas.mpl_connect('button_press_event', on_click)
+        plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    bif = BifurcationDiagram()
+    bif.plot()
+    
